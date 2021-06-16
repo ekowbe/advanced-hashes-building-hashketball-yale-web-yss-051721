7b2e200411dbfbe320a54f49d922c52c76517d67
funny-recipe-0681 <ekow.bentsi-enchill@yale.edu>
2021-06-16 17:24:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -85,16 +85,17 @@ def game_hash
         },
         
         {
-          player_name: "Bismak Biyombo",
+          player_name: "Bismack Biyombo",
           number: 0,
           shoe: 16,
           points: 12,
           rebounds: 4,
           assists: 7,
-          steals: 7,
+          steals: 22,
           blocks: 15,
           slam_dunks: 10,
         },
+        
         {
           player_name: "DeSagna Diop",
           number: 2,
@@ -106,6 +107,7 @@ def game_hash
           blocks: 5,
           slam_dunks: 5,
         },
+        
         {
           player_name: "Ben Gordon",
           number: 8,
@@ -117,14 +119,15 @@ def game_hash
           blocks: 1,
           slam_dunks: 0,
         },
+        
         {
-          player_name: "Brendan Haywood",
+          player_name: "Kemba Walker",
           number: 33,
           shoe: 15,
           points: 6,
           rebounds: 12,
           assists: 12,
-          steals: 22,
+          steals: 7,
           blocks: 5,
           slam_dunks: 12,
         },
